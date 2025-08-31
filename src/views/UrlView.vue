95cc586ef272d25f6df0b90da44c253ae58d98a8
mateus-lopes <mateusalbano22@gmail.com>
2025-08-31 06:19:33 -0300

    feat: add author

--- src/views/UrlView.vue
+++ src/views/UrlView.vue
@@ -71,6 +71,12 @@
         >
           Salvar Url
         </button>
+  <footer class="text-sm mt-4 text-gray-500">
+    <p>
+      2025 | 
+      <a href="https://github.com/mateus-lopes" target="_blank" class="hover:text-blue-500">mateus-lopes</a>
+    </p>
+  </footer>
       </div>
     </div>
     <url-list />
